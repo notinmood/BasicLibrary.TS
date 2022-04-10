f6193743d0329868f81ac66f8fb989760edb881e
解大劦 <9727005@qq.com>
2022-04-10 16:58:47 +0800

    docs📝:  添加README.md文档

--- .test/StringHelper.test.ts
+++ .test/StringHelper.test.ts
@@ -8,8 +8,6 @@ import { expect } from 'chai';
 import { StringHelper as sh } from '../src/index';
 
 
-
-
 describe("字符串测试", function () {
   it("单元测试方法 explode", function () {
     const whole = "qingdao city";
@@ -18,6 +16,18 @@ describe("字符串测试", function () {
     const expectData = 'qingdao';
     expect(actualData).equals(expectData);
   });
+});
 
 
+describe("单元测试包 packageName", function () {
+  it("单元测试方法 funcName", function () {
+    const actualData = 0;
+    const expectData = 0;
+    expect(actualData).equals(expectData);
+  });
+  it("单元测试方法 funcName", function () {
+    const actualData = 0;
+    const expectData = 0;
+    expect(actualData).equals(expectData);
+  });
 });
---
+++ .test/_README.md
@@ -0,0 +1,5 @@
+# 说明
+
+在终端运行命令 `npm run tests`,开启单元测试。
+
+> 单元测试的配置过程，参考 https://www.cnblogs.com/jaxu/archive/2021/10/07/15374686.html
