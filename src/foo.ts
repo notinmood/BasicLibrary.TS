cf49a18076deef76a2d0d497a5673468ffed6a2b
xiedali <9727005@qq.com>
2024-12-16 00:17:06 +0800

    feat: MISC

---
+++ src/foo.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author       : Shandong Xiedali
+ * @Mail         : 9727005@qq.com
+ * @Date         : 2024/12/15 22:56:36
+ * @FilePath     : foo.ts
+ * @Description  :
+ * Copyright (c) 2024 by Hiland & RainyTop, All Rights Reserved.
+ */
+export class Foo {
+
+    static m1 = (someParam: string) => {
+        return "m1" + someParam;
+    }
+
+    static m2(someParam: string) {
+        return "m2" + someParam;
+    }
+}
